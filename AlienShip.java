1a1286fa94d6654e1c4b21473a3c82529825593e
fachrulpbm <fachrul.pbm@polindra.ac.id>
2019-01-16 05:52:25 +0700

    alienship programmer A

---
+++ AlienShip.java
@@ -0,0 +1,9 @@
+public class AlienShip{
+    private String shipName;
+    public AlienShip(String shipName){
+        this.shipName = shipName;
+    }
+    public String getShipName(){
+        return shipName;
+    }
+}
